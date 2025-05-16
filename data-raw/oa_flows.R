f74e3ee9616afbeddc557f3dbec653fdc60eaf23
Hussein Mahfouz <45176416+Hussein-Mahfouz@users.noreply.github.com>
2025-05-16 14:38:52 +0100

    naming update

--- data-raw/oa_flows.R
+++ data-raw/oa_flows.R
@@ -3,81 +3,82 @@ library(geographr)
 library(od)
 library(sf)
 
-# ----- Loading datasets
+# ---------------------
+# 1. Download and unzip OD flow data
+# ---------------------
+zip_url <- "https://www.nomisweb.co.uk/output/census/2021/odwp01ew.zip"
+zip_file <- "data-raw/odwp01ew.zip"
+target_file <- "ODWP01EW_MSOA.csv"
 
-# 1. census 2021 od data
-url <- "https://www.nomisweb.co.uk/output/census/2021/odwp01ew.zip"
-zip_path <- "data-raw/odwp01ew.zip"
-
-if (!file.exists(zip_path)) {
-  download.file(url, zip_path, mode = "wb")
+# Download only if file doesn't exist
+if (!file.exists(zip_file)) {
+  download.file(zip_url, zip_file, mode = "wb")
 }
 
-zip_contents <- unzip(zip_path, list = TRUE)
-print(zip_contents$Name)
+# Unzip only the required CSV
+unzip(zip_file, files = target_file, exdir = "data-raw")
 
-target_file <- "ODWP01EW_MSOA.csv"  # Replace with the actual filename
-unzip(zip_path, files = target_file, exdir = "data-raw")
+# Load OD flow data (Census 2021, MSOA level)
+od_raw <- read_csv(file.path("data-raw", target_file))
 
-msoa_flows <- read_csv(file.path("data-raw", target_file))
+# ---------------------
+# 2. Load spatial and lookup data
+# ---------------------
 
-# 2. OA shapefile 
-msoa_boundaries <- geographr::boundaries_msoa21 
-# remove unnecessary columns
-msoa_boundaries = msoa_boundaries |>
+# Load MSOA 2021 boundaries (geometry only)
+msoa_geom <- geographr::boundaries_msoa21 |>
   select(msoa21_code, geometry)
 
-
-# 3. OA to MSOA to Region lookup 
-lookup = geographr::lookup_postcode_oa_lsoa_msoa_ltla_2025
-
-lookup = lookup |>
+# Load MSOA-to-LTLA lookup table
+lookup <- geographr::lookup_postcode_oa_lsoa_msoa_ltla_2025 |>
   select(msoa21_code, ltla24_code) |>
   distinct(msoa21_code, .keep_all = TRUE) 
 
+# ---------------------
+# 3. Preprocess OD flows
+# ---------------------
 
-# ----- Preprocess
-
-# keep only people commuting in the UK
-msoa_flows = msoa_flows |>
-  filter(`Place of work indicator (4 categories) label` == "Working in the UK but not working at or from home")
-
-# keep only necessary columns 
-msoa_flows = msoa_flows |>
-  select(`Middle layer Super Output Areas code`, `MSOA of workplace code`, Count) |>
-  rename(
+# Filter to include only workers commuting within the UK (exclude home workers)
+flows <- od_raw |>
+  filter(`Place of work indicator (4 categories) label` == 
+           "Working in the UK but not working at or from home") |>
+  select(
     origin = `Middle layer Super Output Areas code`,
     destination = `MSOA of workplace code`,
     count = Count
   )
 
-# join lookup table by origin and then destination
-msoa_flows = msoa_flows |>
+# Join lookup to add LTLA codes for origin and destination
+flows <- flows |>
   left_join(lookup, by = c("origin" = "msoa21_code")) |>
-  rename(ltla24_code_origin = ltla24_code) |>
+  rename(origin_ltla = ltla24_code) |>
   left_join(lookup, by = c("destination" = "msoa21_code")) |>
-  rename(ltla24_code_destination = ltla24_code) 
+  rename(dest_ltla = ltla24_code)
 
+# ---------------------
+# 4. Subset for Leeds only
+# ---------------------
 
 # Leeds LTLA code: E08000035
-msoa_flows_leeds = msoa_flows |>
-  filter(ltla24_code_origin == "E08000035", ltla24_code_destination == "E08000035") |>
-  select(-c(ltla24_code_origin, ltla24_code_destination)) 
+flows_leeds <- flows |>
+  filter(origin_ltla == "E08000035", dest_ltla == "E08000035") |>
+  select(origin, destination, count)
 
-# Keep only boundaries of MSOAs in the flows
-msoa_boundaries = msoa_boundaries |>
-  filter(msoa21_code %in% unique(c(msoa_flows_leeds$origin, msoa_flows_leeds$destination)))
+# Subset MSOA boundaries used in Leeds flows
+zones_leeds <- msoa_geom |>
+  filter(msoa21_code %in% c(flows_leeds$origin, flows_leeds$destination))
 
+# ---------------------
+# 5. Convert OD table to spatial lines using `od` package
+# ---------------------
+flows_leeds <- od::od_to_sf(flows_leeds, zones_leeds)
 
-# use od package to create od object using msoa_flows and zones layer
-msoa_flows_leeds_sf = od::od_to_sf(x = msoa_flows_leeds, z = msoa_boundaries)
-
-# jitter
+# ---------------------
+# 6. Jitter OD data
+# ---------------------
 # TODO
 
-
-
-
-
-# ----- Save cleaned data to data/ folder for package use
-#usethis::use_data(raw_data, overwrite = TRUE)
+# ---------------------
+# 6. Save dataset for internal or exported use
+# ---------------------
+usethis::use_data(flows_leeds, overwrite = TRUE)
